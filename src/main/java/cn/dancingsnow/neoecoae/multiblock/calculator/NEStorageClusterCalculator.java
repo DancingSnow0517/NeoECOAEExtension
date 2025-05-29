df9623c0ec407c889c5b8e81a5aa934a17199d3d
DancingSnow <1121149616@qq.com>
2025-05-30 03:50:58 +0800

    storage calculator

--- src/main/java/cn/dancingsnow/neoecoae/multiblock/calculator/NEStorageClusterCalculator.java
+++ src/main/java/cn/dancingsnow/neoecoae/multiblock/calculator/NEStorageClusterCalculator.java
@@ -3,6 +3,7 @@ package cn.dancingsnow.neoecoae.multiblock.calculator;
 import cn.dancingsnow.neoecoae.blocks.entity.ECOStorageSystemBlockEntity;
 import cn.dancingsnow.neoecoae.blocks.entity.NEBlockEntity;
 import cn.dancingsnow.neoecoae.multiblock.cluster.NEStorageCluster;
+import cn.dancingsnow.neoecoae.util.MultiBlockUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 
@@ -18,15 +19,21 @@ public class NEStorageClusterCalculator extends NEClusterCalculator<NEStorageClu
 
     @Override
     public boolean verifyInternalStructure(ServerLevel level, BlockPos min, BlockPos max) {
-        BlockPos controllerBlockPos = null;
-        for (BlockPos blockPos : BlockPos.betweenClosed(min, max)) {
-            if (level.getBlockEntity(blockPos) instanceof ECOStorageSystemBlockEntity) {
-                 controllerBlockPos = blockPos;
-            }
-        }
-        if (controllerBlockPos == null) {
+        int xSize = max.getX() - min.getX();
+        int ySize = max.getY() - min.getY();
+        int zSize = max.getZ() - min.getZ();
+
+        if (ySize != 3) {
             return false;
         }
+        ECOStorageSystemBlockEntity controller;
+        for (BlockPos pos : MultiBlockUtil.allPossibleController(min, max)) {
+            if (level.getBlockEntity(pos) instanceof ECOStorageSystemBlockEntity be) {
+                controller = be;
+                break;
+            }
+        }
+
         return false;
     }
 }
---
+++ src/main/java/cn/dancingsnow/neoecoae/util/MultiBlockUtil.java
@@ -0,0 +1,40 @@
+package cn.dancingsnow.neoecoae.util;
+
+import lombok.NoArgsConstructor;
+import net.minecraft.core.BlockPos;
+
+import java.util.Set;
+
+@NoArgsConstructor
+public class MultiBlockUtil {
+    public static Set<BlockPos> allPossibleController(BlockPos min, BlockPos max) {
+        int xSize = max.getX() - min.getX();
+        int zSize = max.getZ() - min.getZ();
+
+        if (xSize > zSize && zSize == 2) {
+            return allXPossibleController(min, max);
+        }
+        if (zSize > xSize && xSize == 2) {
+            return allYPossibleController(min, max);
+        }
+        return Set.of();
+    }
+
+    private static Set<BlockPos> allXPossibleController(BlockPos min, BlockPos max) {
+        return Set.of(
+            new BlockPos(min.getX() + 1, min.getY() + 1, min.getZ()),
+            new BlockPos(min.getX() + 1, min.getY() + 1, min.getZ() + 1),
+            new BlockPos(max.getX() - 1, min.getY() + 1, min.getZ()),
+            new BlockPos(max.getX() - 1, min.getY() + 1, min.getZ() + 1)
+        );
+    }
+
+    private static Set<BlockPos> allYPossibleController(BlockPos min, BlockPos max) {
+        return Set.of(
+            new BlockPos(min.getX(), min.getY() + 1, min.getZ() + 1),
+            new BlockPos(min.getX() + 1, min.getY() + 1, min.getZ() + 1),
+            new BlockPos(min.getX(), min.getY() + 1, max.getZ() - 1),
+            new BlockPos(min.getX() + 1, min.getY() + 1, max.getZ() - 1)
+        );
+    }
+}
