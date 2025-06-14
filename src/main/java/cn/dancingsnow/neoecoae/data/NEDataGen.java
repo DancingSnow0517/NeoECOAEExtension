d42faf0990678cbb2c1affabc6a5c54e7dedde95
DancingSnow <1121149616@qq.com>
2025-06-15 00:46:25 +0800

    添加群系结构生成

--- src/main/java/cn/dancingsnow/neoecoae/data/NEDataGen.java
+++ src/main/java/cn/dancingsnow/neoecoae/data/NEDataGen.java
@@ -2,16 +2,35 @@ package cn.dancingsnow.neoecoae.data;
 
 import cn.dancingsnow.neoecoae.data.lang.NELangGenerator;
 import cn.dancingsnow.neoecoae.data.model.CellModelGenerator;
+import cn.dancingsnow.neoecoae.data.provider.NERegistryProvider;
 import cn.dancingsnow.neoecoae.data.recipe.NERecipeGenerator;
 import cn.dancingsnow.neoecoae.registration.provider.NEProviderTypes;
 import com.tterrag.registrate.providers.ProviderType;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
 
 import static cn.dancingsnow.neoecoae.NeoECOAE.REGISTRATE;
 
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class NEDataGen {
     public static void configureDataGen() {
         REGISTRATE.addDataGenerator(NEProviderTypes.CELL_MODEL, CellModelGenerator::accept);
         REGISTRATE.addDataGenerator(ProviderType.LANG, NELangGenerator::accept);
         REGISTRATE.addDataGenerator(ProviderType.RECIPE, NERecipeGenerator::accept);
+    }
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        CompletableFuture<HolderLookup.Provider> registries = event.getLookupProvider();
+        PackOutput packOutput = generator.getPackOutput();
+
+        generator.addProvider(event.includeServer(), new NERegistryProvider(packOutput, registries));
     }
 }
---
+++ src/main/java/cn/dancingsnow/neoecoae/data/worldgen/NEBiomeModifiers.java
@@ -0,0 +1,23 @@
+package cn.dancingsnow.neoecoae.data.worldgen;
+
+import cn.dancingsnow.neoecoae.NeoECOAE;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class NEBiomeModifiers {
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        HolderGetter<Biome> biomes = context.lookup(Registries.BIOME);
+        HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+    }
+
+    private static ResourceKey<BiomeModifier> key(String id) {
+        return ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, NeoECOAE.id(id));
+    }
+}
