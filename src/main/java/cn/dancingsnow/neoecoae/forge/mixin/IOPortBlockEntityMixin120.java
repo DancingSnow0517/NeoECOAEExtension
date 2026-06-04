89d9c059a5182e0e33b3f0b0159baedfd247ddaf
Yang120231 <hnhjyang@163.com>
2026-06-04 09:57:37 +0800

    去除1.21.1多余Mixin

---
+++ src/main/java/cn/dancingsnow/neoecoae/forge/mixin/IOPortBlockEntityMixin120.java
@@ -0,0 +1,30 @@
+package cn.dancingsnow.neoecoae.forge.mixin;
+
+import appeng.api.storage.cells.ISaveProvider;
+import appeng.api.storage.cells.StorageCell;
+import appeng.blockentity.storage.IOPortBlockEntity;
+import cn.dancingsnow.neoecoae.api.storage.ECOStorageCells;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = IOPortBlockEntity.class, remap = false)
+public class IOPortBlockEntityMixin120 {
+    @WrapOperation(
+        method = "tickingRequest",
+        at = @At(
+            value = "INVOKE",
+            target = "Lappeng/api/storage/StorageCells;getCellInventory(Lnet/minecraft/world/item/ItemStack;Lappeng/api/storage/cells/ISaveProvider;)Lappeng/api/storage/cells/StorageCell;"
+        )
+    )
+    private StorageCell neoecoae$getEcoCellInventory(
+        ItemStack stack,
+        ISaveProvider saveProvider,
+        Operation<StorageCell> original
+    ) {
+        StorageCell aeCell = original.call(stack, saveProvider);
+        return aeCell != null ? aeCell : ECOStorageCells.getCellInventory(stack, saveProvider);
+    }
+}
