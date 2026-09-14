c241411c04c61dfc43b99ebd4d61c5b3b0a35d4e
Yang120231 <hnhjyang@163.com>
2026-09-14 19:45:06 +0800

    test: add host texture coverage

--- src/test/java/cn/dancingsnow/neoecoae/client/ECOPortResourcesTest.java
+++ src/test/java/cn/dancingsnow/neoecoae/client/ECOPortResourcesTest.java
@@ -8,6 +8,8 @@ import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
 
 class ECOPortResourcesTest {
+    private static final String GUI_TEXTURES = "/assets/neoecoae/textures/gui/";
+
     @Test
     void confirmationStyleAndReferencedBackgroundArePackaged() throws Exception {
         String styleResource = "/assets/ae2/screens/eco_planner_report.json";
@@ -28,6 +30,35 @@ class ECOPortResourcesTest {
                 assertEquals(
                         style.getAsJsonObject("background").get("textureHeight").getAsInt(), bitmap.getHeight());
             }
+        }
+    }
+
+    @Test
+    void highVersionHostAndButtonTexturesArePackagedAtReferenceDimensions() throws Exception {
+        assertTexture("background.png", 16, 16);
+        assertTexture("ae2_121/eco_button.png", 200, 20);
+        assertTexture("ae2_121/eco_button_highlighted.png", 200, 20);
+        assertTexture("ae2_121/eco_button_disabled.png", 200, 20);
+        assertTexture("ae2_121/eco_states.png", 256, 256);
+        assertTexture("slot.png", 18, 18);
+        assertTexture("eco_button_slot_up.png", 23, 30);
+        assertTexture("eco_button_slot_middle.png", 23, 24);
+        assertTexture("eco_button_slot_down.png", 23, 27);
+        assertTexture("eco_mirrored_button_slot_up.png", 23, 30);
+        assertTexture("eco_mirrored_button_slot_middle.png", 23, 24);
+        assertTexture("eco_mirrored_button_slot_down.png", 23, 27);
+        assertTexture("eco_extra_panels.png", 80, 80);
+        assertTexture("eco_large_integrated_working_station.png", 256, 256);
+        assertTexture("eco_nbtbench.png", 256, 256);
+    }
+
+    private void assertTexture(String name, int width, int height) throws Exception {
+        try (var image = getClass().getResourceAsStream(GUI_TEXTURES + name)) {
+            assertNotNull(image, name);
+            var bitmap = javax.imageio.ImageIO.read(image);
+            assertNotNull(bitmap, name);
+            assertEquals(width, bitmap.getWidth(), name);
+            assertEquals(height, bitmap.getHeight(), name);
         }
     }
 }
